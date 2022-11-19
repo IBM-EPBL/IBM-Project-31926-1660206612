9a33050d0d4bf7b1d5287c6efcd87031827c9699
Kalpika K <69431196+kalpikak@users.noreply.github.com>
2022-11-19 17:21:20 +0530

    Add files via upload

--- Project Development Phase/Sprint-3/application development/app.py
+++ Project Development Phase/Sprint-3/application development/app.py
@@ -1,3 +1,5 @@
+from time import sleep
+import time
 from flask import Flask,render_template,request
 # Flask-It is our framework which we are going to use to run/serve our application.
 #request-for accessing file which was uploaded by the user on our application.
